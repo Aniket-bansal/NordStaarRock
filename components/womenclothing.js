e546052fbb743b14222e0cea74752be934e55da5
Aniket bansal <77097101+Aniket-bansal@users.noreply.github.com>
2022-05-07 13:27:36 +0530

    Add files via upload

---
+++ components/womenclothing.js
@@ -0,0 +1,124 @@
+function womensclothing(){
+    return `
+    
+    <div id="womenscloths">
+    <ul type="none"> Women: Get Inspired
+  <li>Mother's Day Gifts</li>
+  <li>New Arrivals</li>
+   <li>Best Sellers</li>
+   <li>Designer Brands</li>
+   <li>Contemporary Brands</li>
+   <li>Trend Brands</li>
+  <li> Dresses Under $50</li>
+  <li>Denim Under $50</li>
+  <li>Swim & Sun Under $50</li>
+  <li>Sandals Under $50</li>
+  <li>Wedding Shop</li>
+  <li>Shop by Occasion</li>
+  <li>Shop by Trend</li>
+  <li>Nike for the Family</li>
+  <li>Rack Essentials</li>
+  <li>Shop By Brand</li>
+ </ul>
+  </div>
+  <div id="womenscloths">
+  <ul type="none">Clothing
+  <li>Activewear</li>
+  <li>Blazers</li>
+  <li>Coats & Jackets</li>
+  <li>Dresses</li>
+  <li>Jeans & Denim</li>
+  <li>Jumpsuits & Rompers</li>
+  <li>Lingerie, Hosiery & Shapewear</li>
+  <li>Loungewear</li>
+  <li>Pants & Leggings</li>
+  <li>Shorts</li>
+  <li>Skirts</li>
+  <li>Sleepwear & Robes</li>
+  <li>Sweaters</li>
+  <li>Sweatshirts & Hoodies</li>
+  <li>Swimsuits & Cover-Ups</li>
+  <li>Tops</li>
+  <li>Plus-Size</li>
+
+  </ul>
+  </div>
+   
+  <div id="womenscloths">
+  <ul type="none">Shoes
+  <li>Athletic & Running</li>
+  <li>Boots & Booties</li>
+  <li>Clogs</li>
+  <li>Comfort</li>
+  <li>Espadrilles</li>
+  <li>Flats</li>
+  <li>Flip-Flops & Slides</li>
+  <li>Heels</li>
+  <li>Loafers & Oxfords</li>
+  <li>Mules</li>
+  <li>Rain Boots</li>
+  <li>Sandals</li>
+  <li>Slippers</li>
+  <li>Sneakers</li>
+  <li>Wedges</li>
+  <li>Extended Shoe Sizes & Widths</li>
+  </ul>
+  </div>
+  <div id="womenscloths">
+  <ul type="none">Young Adults
+  <li>Accessories</li>
+  <li>Clothing</li>
+  <li>Shoes</li>
+  </ul>
+  <br><br>
+  <ul type="none">Handbags
+  <li>Designer Bags</li>
+  </ul>
+  <br>
+  <ul type="none">Sunglasses & Eyewear</ul>
+  <br>
+
+  <ul type="none">Jewelry</ul>
+  <br>
+
+  <ul type="none">Watches</ul>
+  <br>
+
+  <ul type="none">Accessories</ul>
+  <br>
+
+  <ul type="none">Luggage & travel</ul>
+  <br>
+
+  <ul type="none">Beauty</ul>
+  </div>
+  
+
+  <div id="womenscloths">
+  <ul type="none">Activewear
+  <li>Crops & Capris</li>
+  <li>Jackets</li>
+  <li>Leggings</li>
+  <li>Shirts & Tees</li>
+  <li>Shorts & Skirts</li>
+  <li>Sports Bras</li>
+  <li>Sweatpants & Joggers</li>
+  <li>Sweatshirts & Hoodies</li>
+  <li>Tanks</li>
+  </ul>
+  <br><br>
+  <ul type="none">Active Shoes
+  <li>Hiking & Trail</li>
+  <li>Running</li>
+  <li>Training</li>
+  <li>Walking</li>
+
+  </ul>
+
+   <br>
+   <ul>Athletic Gear & Equipment</ul>
+  </div>
+  
+    `
+}
+export { womensclothing };
