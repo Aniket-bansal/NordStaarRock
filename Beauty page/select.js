e07e1e8ffe4da2473b7158c5c17bed1ffef15d20
Pritu2022 <101568121+Pritu2022@users.noreply.github.com>
2022-05-08 11:43:23 +0530

    Add files via upload

---
+++ Beauty page/select.js
@@ -0,0 +1,11 @@
+function select () {
+    return `<select  id="select">
+    <option value="none">Select-Size</option>
+    <option value="">Small</option>
+    <option value="">Medium</option>
+    <option value="">Large</option>
+    <option value="">Extra Large</option>
+</select>`
+}
+
+export default select;
